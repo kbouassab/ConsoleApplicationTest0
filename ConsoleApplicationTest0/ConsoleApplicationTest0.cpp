3cf728e8ec31337b61a281f091fff942d04a5074
kbouassab <khalid.bouassab@gmail.com>
2014-06-19 23:49:42 +0100

    comit

---
+++ ConsoleApplicationTest0/ConsoleApplicationTest0.cpp
@@ -0,0 +1,13 @@
+// ConsoleApplicationTest0.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	int x=10;
+	int y=10;
+	return 0;
+}
+
